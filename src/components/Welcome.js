6785cefe4fe735e712f337efc478b0283dfdd2a3
vaishaliravasoambi <65502403+vaishaliravasoambi@users.noreply.github.com>
2020-11-09 11:06:21 +0530

    ch

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,10 @@
 // write code for Welcome component here
+import React from "react";
+export default function Welcome(props) {
+  return (
+    <>
+      <h1>Hey {props.name}!</h1>
+      <h2>Welcome to Newton School</h2>
+    </>
+  );
+}
